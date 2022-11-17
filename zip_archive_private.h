184a0538373b8609c89b3e3ce3c66683e2ad191a
Yurii Zubrytskyi <zyy@google.com>
2022-11-17 16:07:36 +0000

    Revert "[zip] Use memory mapping when possible"
    Revert submission 2303933
    
    Reason for revert: b/259513165 - breaking the boot test
    Reverted Changes:
    Ia68343577:[zip] Final zero copy part - direct writer buffer
    I5e2b50e84:[zip] Use memory mapping when possible
    
    Change-Id: I7d4f02e1078195b687b7c31a49c90e45ea19dc82

--- zip_archive_private.h
+++ zip_archive_private.h
@@ -34,10 +34,17 @@
 
 class MappedZipFile {
  public:
-  explicit MappedZipFile(int fd, off64_t length = -1, off64_t offset = 0);
+  explicit MappedZipFile(const int fd)
+      : has_fd_(true), fd_(fd), fd_offset_(0), base_ptr_(nullptr), data_length_(-1) {}
+
+  explicit MappedZipFile(const int fd, off64_t length, off64_t offset)
+      : has_fd_(true), fd_(fd), fd_offset_(offset), base_ptr_(nullptr), data_length_(length) {}
 
   explicit MappedZipFile(const void* address, size_t length)
-      : base_ptr_(address), data_length_(static_cast<off64_t>(length)) {}
+      : has_fd_(false), fd_(-1), fd_offset_(0), base_ptr_(address),
+        data_length_(static_cast<off64_t>(length)) {}
+
+  bool HasFd() const { return has_fd_; }
 
   int GetFileDescriptor() const;
 
@@ -50,13 +57,17 @@ class MappedZipFile {
   const uint8_t* ReadAtOffset(uint8_t* buf, size_t len, off64_t off) const;
 
  private:
-  std::unique_ptr<android::base::MappedFile> mapped_file_;
+  // If has_fd_ is true, fd is valid and we'll read contents of a zip archive
+  // from the file. Otherwise, we're opening the archive from a memory mapped
+  // file. In that case, base_ptr_ points to the start of the memory region and
+  // data_length_ defines the file length.
+  const bool has_fd_;
 
-  const int fd_ = -1;
-  const off64_t fd_offset_ = 0;
+  const int fd_;
+  const off64_t fd_offset_;
 
-  const void* base_ptr_ = nullptr;
-  mutable off64_t data_length_ = -1;
+  const void* const base_ptr_;
+  mutable off64_t data_length_;
 };
 
 class CentralDirectory {
